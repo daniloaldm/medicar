d8a2c5914893d97bd6b76e961f6565d774540abc
Danilo Alexandrino <danilo@hibrido.com.br>
2020-11-20 00:22:28 -0300

    Adicionando dropdowns de teste estaticos

--- frontend/src/app/layout/consultation/consultation.component.ts
+++ frontend/src/app/layout/consultation/consultation.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+//teste
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-consultation',
   templateUrl: './consultation.component.html',
   styleUrls: ['./consultation.component.css']
 })
 export class ConsultationComponent implements OnInit {
+
+  //teste
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
   constructor() { }
 
